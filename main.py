b512d4cd464f915ce580a683e9589ad6801d2f32
HongQuan2003 <148517311+HongQuan2003@users.noreply.github.com>
2023-11-29 09:14:29 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+
+
+st.title("Data Visualization")
+
+st.header("upload data")
+
+data_file = st.file_uploader("choose a csv file")
+
+if data_file is not None:
+    df = pd.read_csv(data_file)
+  
+    st.header("Show data")
+    st.dataframe(df)
+    
+    st.header("Des")
+    st.table(df.describe())
+    
+    st.header("Info")
+    buffer = io.stringIO()
+    df.info(buf=buffer)
+    st.text(buffer.getvalue())
